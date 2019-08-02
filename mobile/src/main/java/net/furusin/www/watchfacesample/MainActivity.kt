b44ead323a064f540d51409f8d4dd3379c59bd6a
ShinjiFurukawa <furukawa.shinji@furyu.jp>
2019-08-02 19:29:21 +0900

    端末ローカルの画像を取得して表示できるようにした

--- mobile/src/main/java/net/furusin/www/watchfacesample/MainActivity.kt
+++ mobile/src/main/java/net/furusin/www/watchfacesample/MainActivity.kt
@@ -1,28 +1,52 @@
 package net.furusin.www.watchfacesample
 
+import android.app.Activity
 import android.content.Intent
-import android.content.IntentFilter
-import android.os.BatteryManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.provider.MediaStore
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import java.io.IOException
 
 private val TAG = MainActivity::class.java.simpleName
+private const val READ_REQUEST_CODE = 1
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG, "battery Level = ${getBatteryLevel()}")
+        findViewById<Button>(R.id.select_image_button).setOnClickListener {
+            getImage()
+        }
     }
 
-    private fun getBatteryLevel(): Int? {
-        return registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED))?.let { batteryInfo ->
-            val max = batteryInfo.getIntExtra(BatteryManager.EXTRA_SCALE, -1).toDouble()
-            val level = batteryInfo.getIntExtra(BatteryManager.EXTRA_LEVEL, -1).toDouble()
+    /**
+     * 端末に保存されている画像を取得する
+     */
+    private fun getImage() {
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "image/*"
+        }
 
-            return@let (level / max * 100).toInt()
+        startActivityForResult(intent, READ_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            if (data == null) {
+                return
+            }
+
+            try {
+                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, data.data)
+                findViewById<ImageView>(R.id.imageView).setImageBitmap(bitmap)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
         }
     }
 }
